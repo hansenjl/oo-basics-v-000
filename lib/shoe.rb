08f3640288ed346b7a267e8a27f0bccac6f02247
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-03-13 21:56:33 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,25 +1,3 @@
 class Shoe
-  def initialize(brand)
-    @brand =  brand
-  end
-  def brand
-    @brand
-  end
-  def color=(color)
-    @color = color
-  end
-  def color
-    @color
-  end
-  def size=(size)
-    @size = size
-  end
-  def size
-    @size
-  end
-  attr_accessor :condition, :material
-  def cobble
-    @condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+
 end
